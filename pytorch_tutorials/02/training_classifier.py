a13c04bf463a68408fad7836a277ee3998c3ab5a
lsmman <tmdgus507@naver.com>
2018-08-07 20:52:03 +0900

    torch_tutorial_4rd_train_02_existing_error

--- pytorch_tutorials/02/training_classifier.py
+++ pytorch_tutorials/02/training_classifier.py
@@ -1,35 +1,48 @@
+
+
 import torch
 import torchvision
 import torchvision.transforms as transforms
 import matplotlib.pyplot as plt
 import numpy as np
 
-# 1. Loading and normalizing CIFAR10
 
-transform = transforms.Compose(
-    [transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
-)
+def run():
+    torch.multiprocessing.freeze_support()
+    print('loop')
+    #
+    # # 1. Loading and normalizing CIFAR10
+    #
+    transform = transforms.Compose(
+        [transforms.ToTensor(),
+         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
+    )
 
-trainset = torchvision.datasets.CIFAR10(root='../data', train=True,
-                                        download=True, transform=transforms)
+    trainset = torchvision.datasets.CIFAR10(root='../data', train=True,
+                                            download=True, transform=transforms)
 
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
-                                          shuffle=True, num_workers=2)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
+                                              shuffle=True, num_workers=0)
 
-testset = torchvision.datasets.CIFAR10(root='../data', train=False,download=True, transform=transforms)
-testloader = torch.utils.data.DataLoader(testset, batch_size=4,
-                                         shuffle=False, num_workers=2)
+    testset = torchvision.datasets.CIFAR10(root='../data', train=False, download=True, transform=transforms)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=4,
+                                             shuffle=False, num_workers=0)
+    print('done')
 
-classes = ('plane', 'car', 'bird', 'cat',
-           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+    classes = ('plane', 'car', 'bird', 'cat',
+               'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
-def imshow(img):
-    img = img / 2 + 0.5
-    npimg = img.numpy()
-    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    def imshow(img):
+        img = img / 2 + 0.5
+        npimg = img.numpy()
+        plt.imshow(np.transpose(npimg, (1, 2, 0)))
 
-dataiter = iter(trainloader)
-images, labels = dataiter.next()
+    # images, labels = iter(trainloader).next()
+    images, labels = trainset[0]
 
-imshow(torchvision.utils.make_grid(images))
-print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
+    imshow(torchvision.utils.make_grid(images))
+    print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
+
+
+if __name__ == '__main__':
+    run()
